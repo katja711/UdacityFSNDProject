d56a20f03c79472d2b886b06836d1832204f02de
katja <k.kleeberg@protonmail.com>
2022-07-27 22:40:22 +0200

    test to break the pytests

--- test_main.py
+++ test_main.py
@@ -24,7 +24,8 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    assert response.status_code == 200
+    #assert response.status_code == 200
+    assert response.status_code == 300
     assert response.json == 'Healthy'
 
 
